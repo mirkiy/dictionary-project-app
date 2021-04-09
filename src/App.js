d84fb69dd97c37a99db0b03e2a38045739a8df8e
mirkiy <77293514+mirkiy@users.noreply.github.com>
2021-04-09 15:32:35 +0100

    Search engine added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header> Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Mirka Zacharova</footer>
+      </div>
     </div>
   );
 }
